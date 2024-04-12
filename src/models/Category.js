7cf7849e2c43f0b75a30fbeb2ac4a8d6da42477f
TatiStoiev <tatistoiev@hotmail.com>
2024-04-12 09:13:19 -0300

    implementei o model category

---
+++ src/models/Category.js
@@ -0,0 +1,19 @@
+module.exports = (sequelize, DataTypes) => {
+    const Category = sequelize.define('Category', {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true,
+            allowNull: false,
+          }, 
+          name: {
+          type: DataTypes.STRING(255),
+          allowNull: false,
+          }
+    }, {
+        tableName: 'categories', 
+        timestamps: false, 
+        underscored: true,
+    })
+    return Category;
+};
