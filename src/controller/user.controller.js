29198587a9add31bc6137e62d326c6ac27fc02de
TatiStoiev <tatistoiev@hotmail.com>
2024-04-11 22:22:09 -0300

    implementei a rota post user com as funções de controller, service e middleware

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -20,10 +20,35 @@ const login = async (req, res) => {
 
   const token = jwt.sign({ data: { email } }, secret, jwtConfig);
   const status = 'SUCCESSFUL';
-  console.log('token no controller', token);
+  return res.status(mapStatusHttp(status)).json({ token });
+};
+
+const addUser = async (req, res) => {
+  const { email } = req.body;
+  const profile = req.body;
+
+  const emailRegistered = await userServices.emailAlreadyExists(email);
+  
+  if (emailRegistered) {
+    const status = 'USER_REGISTERED';
+    return res.status(mapStatusHttp(status)).json({ message: 'User already registered' });
+  } 
+  
+  await userServices.addUser(profile);
+
+  const secret = process.env.JWT_SECRET;
+
+  const jwtConfig = {
+    expiresIn: '1h',
+    algorithm: 'HS256',
+  }; 
+
+  const token = jwt.sign({ data: { email } }, secret, jwtConfig);
+  const status = 'REGISTERED';
   return res.status(mapStatusHttp(status)).json({ token });
 };
 
 module.exports = {
   login,
+  addUser,
 };
