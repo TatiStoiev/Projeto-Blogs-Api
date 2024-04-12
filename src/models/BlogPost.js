c145ed8675082a68bc8660a4d687eaf6613ac4f2
TatiStoiev <tatistoiev@hotmail.com>
2024-04-12 11:54:42 -0300

    implementei a model de BlogPost e o relacinamento dela com a model user

---
+++ src/models/BlogPost.js
@@ -0,0 +1,40 @@
+module.exports = (Sequelize, DataTypes) => {
+    const BlogPost = Sequelize.define('BlogPost', {
+        id: {
+            type: DataTypes.INTEGER,
+            autoIncrement: true,
+            allowNull: false,
+            primaryKey:true,
+          },
+          title: {
+            type: DataTypes.STRING(255), 
+          },
+          content: {
+            type: DataTypes.STRING(255),
+          },
+          userId: {
+            type: DataTypes.INTEGER,
+            references: {
+              model: 'users',
+              key: 'id',
+            }
+          },
+          published: {
+            type: DataTypes.DATE,
+          },
+          updated: {
+            type: DataTypes.DATE,
+          },
+          tableName: 'categories', 
+          timestamps: false, 
+          underscored: true,
+
+    })
+    BlogPost.associate = (models) => {
+        BlogPost.belongsTo(models.User, {
+            foreignKey: 'userId', 
+            as: 'User'
+        }) 
+    }
+    return BlogPost;
+};
